6c80fd292da0168a08cbae42ed3cb4ba9da11de6
EmanuellePortillo <emanuelle.portillo@hotmail.com>
2014-05-16 09:46:55 -0600

    modifique el login para que quedara mejor, ya solo lo terminas de acomodar mijo :)

--- login.php
+++ login.php
@@ -1,12 +1,15 @@
-<div class="mensaje">Login</div><div class="mostrar">
+<div class="mensaje">Login</div>
+<div class="mostrar form">
     <form action="entrar.php" method="POST">
         <fieldset class="login">
-		    <legend>Login</legend>
-		    <label for="Usuario">Usuario:</label>
-		    <input name="usuario" id="Usuario" type="text"/><br/>
-		    <label for="Password">Password:</label>
-		    <input name="pass" id="Password" type="password"/>
-		    <br/>
+		    <div style="width: 50%;">
+		        <label for="Usuario">Usuario:</label>
+		        <input name="usuario" id="Usuario" type="text"/>
+            </div>
+            <div style="width: 50%;">
+		        <label for="Password">Password:</label>
+		        <input name="pass" id="Password" type="password"/>
+		    </div>
 	    	<input type="submit" value="Login"/>
             <a href="registrarse.php">registrarse</a>
 	    </fieldset>
